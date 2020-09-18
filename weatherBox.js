895bfbe55c02bf14c918f261660bf5c9cf07e08d
Martin Donovan <donovanm.ny@gmail.com>
2020-09-18 13:18:09 -0400

    Split up the function

--- weatherBox.js
+++ weatherBox.js
@@ -3,27 +3,62 @@
 // Creates a form within the element imageDescription
 // The form contains a label and a text box.
 // The text box is formatted as a read only input form for aesthetics.
+
 function weatherbox(textInput) {
-	let descriptionNode = document.getElementById("imageDescription");
-	descriptionNode.innerHTML =""; // Clear the element
+	ImgServerView.weatherbox(textInput);
+}
+
+var ImgServerView = {
+	weatherbox(textInput) {
+		this.clearElement("imageDescription");
 	
-	let weatherForm = document.createElement("form");
-	descriptionNode.appendChild(weatherForm);
+		let descriptionForm = this.insertForm("imageDescription");
+		
+		let weatherGroup = this.insertFormGroup(descriptionForm);
+		
+		this.insertLabel(weatherGroup, "weatherBox", "Weather");
+		
+		this.insertInputBox(weatherGroup, "weatherBox", "Weather", textInput, true);
+	},
 	
-	let weatherNode = document.createElement("div");
-	weatherNode.className="form-group";
-	weatherForm.appendChild(weatherNode);
+	clearElement(elementId) {
+		let givenElement = document.getElementById(elementId);
+		givenElement.innerHTML =""; // Clear the element
+	},
 	
-	let weatherBoxLabel = document.createElement("label");
-	weatherNode.appendChild(weatherBoxLabel);
-	weatherBoxLabel.setAttribute("for", "weatherBox");
-	weatherBoxLabel.innerHTML = "Weather";
+	insertForm(containerElementId) {
+		let containerElement = document.getElementById(containerElementId);
+		
+		let insertedForm = document.createElement("form");
+		containerElement.appendChild(insertedForm);
+		return (insertedForm);
+	},
 	
-	let weatherBox = document.createElement("input");
-	weatherNode.appendChild(weatherBox);
-	weatherBox.setAttribute("type", "text");
-	weatherBox.setAttribute("id", "weatherBox");
-	weatherBox.className = "form-control";
-	weatherBox.setAttribute("placeholder", textInput);
-	weatherBox.setAttribute("readonly", "readonly");
+	insertFormGroup(insertedForm) {
+		let insertedNode = document.createElement("div");
+		insertedNode.className="form-group";
+		insertedForm.appendChild(insertedNode);
+		return(insertedNode);
+	},
+	
+	insertLabel(group, labeledElement, labelText) {
+		let weatherBoxLabel = document.createElement("label");
+		group.appendChild(weatherBoxLabel);
+		weatherBoxLabel.setAttribute("for", labeledElement);
+		weatherBoxLabel.innerHTML = labelText;
+	},
+	
+	insertInputBox(group, boxId, placeholder, textInput, readOnly) {
+		let insertedBox = document.createElement("input");
+		group.appendChild(insertedBox);
+		insertedBox.setAttribute("type", "text");
+		insertedBox.setAttribute("id", boxId);
+		insertedBox.className = "form-control";
+		insertedBox.setAttribute("placeholder", placeholder);
+		insertedBox.setAttribute("value", textInput);
+		insertedBox.innerHTML ="Test";
+		if (readOnly === true) {
+			insertedBox.setAttribute("readonly", "readonly");
+		}	
+	}
 }
