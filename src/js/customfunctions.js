4ce2a9baec371dd1fc79978faf12932668632a93
Martin Donovan <donovanm.ny@gmail.com>
2020-09-29 19:14:30 -0400

    Put navbar and footer in functions and added to all webpages

--- src/js/customfunctions.js
+++ src/js/customfunctions.js
@@ -26,6 +26,100 @@ var ImgServerModel = {
 
 var ImgServerView = {
     
+    insertFooter() {
+        document.getElementById("footer").innerHTML = "<div class=\"container\">\
+        \
+        <!-- The footer rectangle -->\
+        <div class=\"card border-0 m-2\">\
+            <div class=\"card-body p-2\">\
+            \
+                <!-- The rows and columns of the footer -->\
+                <div class=\"row\">\
+                    <div class=\"col-9\">\
+                        <div class=\"card-text h-100 d-inline-flex align-items-center\">Created by Joyce Sommer, Martin Donovan, Mohit Gokul Murali, Nick Anzalone, and Sean Jones for Fall 2020 CSE 442 at SUNY University at Buffalo.<\/div>\
+                    <\/div>\
+                    \
+                    <!-- text-reset makes the links inherit their color -->\
+                    <div class=\"col-3 text-right\">\
+                        <div>\
+                            <a href=\"loginForm.html\" class=\"text-reset\">Log In<\/a>\
+                        <\/div>\
+                        <div>\
+                            <a href=\"registrationForm.html\" class=\"text-reset\">Sign Up<\/a>\
+                        <\/div>\
+                        <div>\
+                            <a href=\"aboutUs.html\" class=\"text-reset\">About Us<\/a>\
+                        <\/div>\
+                        <div>\
+                            <a href=\"index.html\" class=\"text-reset\">Home<\/a>\
+                        <\/div>\
+                    <\/div>\
+                <\/div>\
+            <\/div>\
+        <\/div>\
+    <\/div>"; 
+    },
+    
+    insertNavbar() {
+     document.getElementById("navbar").innerHTML  = "<!-- The navbar -->\
+        <nav id=\"cseNavbar\" class=\"navbar rounded-pill-bottom\" style=\"background-color: #918D85; color:#fff\">\
+        \
+            <!-- The dropdown menu, just a placeholder for now, pl-5 is there to give the corner space -->\
+            <span class=\"nav-item pl-5\">\
+                <a class=\"nav-link text-reset\" href=\"#collapseMenu\" role=\"button\" data-toggle=\"collapse\" aria-expanded=\"false\" aria-controls=\"collapseMenu\">\
+        \
+                    <!-- Hamburger icon -->\
+                    <svg width=\"2em\" height=\"2em\" viewBox=\"0 0 16 16\" class=\"bi bi-list\" fill=\"currentColor\" xmlns=\"http:\/\/www.w3.org\/2000\/svg\">\
+                        <path fill-rule=\"evenodd\" d=\"M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z\"\/>\
+                    <\/svg>\
+                <\/a>\
+            <\/span>\
+            \
+            <!-- The title, with text-reset to make the links inherit their color, and pr-5 to give the corner space -->\
+            <a href=\"index.html\" class=\"navbar-brand text-reset m-0 h1 pr-5 py-0\" style=\"font-size: 2rem\">UB Image Server<\/a>\
+        <\/nav>\
+        \
+        <!-- The dropdown menu, implemented as a collaped card -->\
+        <div>\
+            <div class=\"collapse\" id=\"collapseMenu\" style=\"position: absolute; z-index:1001;\">\
+                <div class=\"menu-card card-body p-0\">\
+                \
+                <!-- Drop down form -->\
+                    <form class=\"form-row px-2\">\
+                        <div class=\"col-9\">\
+                            <div class=\"form-group my-1 p-1\">\
+                                <label class=\"sr-only\" for=\"menuUsername\">Username<\/label>\
+                                <input type=\"username\" class=\"form-control\" id=\"menuUsername\" placeholder=\"Username\">\
+                            <\/div>\
+                            <div class=\"form-group my-1 p-1\">\
+                                <label class=\"sr-only\" for=\"menuPassword\">Password<\/label>\
+                                <input type=\"password\" class=\"form-control\" id=\"menuPassword\" placeholder=\"Password\">\
+                            <\/div>\
+                        <\/div>\
+                        <div class=\"col-3 d-flex align-items-center\">\
+                            <button type=\"submit\" class=\"btn btn-dark\">\
+                            \
+                                <!-- Play icon -->\
+                                <svg width=\"2em\" height=\"2em\" viewBox=\"0 0 16 16\" class=\"bi bi-play-fill\" fill=\"currentColor\" xmlns=\"http:\/\/www.w3.org\/2000\/svg\">\
+                                    <path d=\"M11.596 8.697l-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z\"\/>\
+                                <\/svg>\
+                            <\/button>\
+                        <\/div>\
+                    <\/form>\
+                    \
+                    <!-- Drop down links -->\
+                    <div class=\"text-left px-3 pb-2\" style=\"font-size: 1rem;\">\
+                        <a class=\"text-reset\" href=\"loginForm.html\">Log In<\/a>&nbsp;|&nbsp;<a class=\"text-reset\" href=\"registrationForm.html\">Create Account<\/a>\
+                    <\/div>\
+                    <div class=\"d-inline-flex align-items-center text-center\">\
+                    <\/div>\
+                    <a class=\"text-reset py-2\" href=\"aboutUs.html\" style=\"font-size: 1.7rem\">About Us<\/a>\
+                    <a class=\"text-reset py-2\" href=\"index.html\" style=\"font-size: 1.7rem\">Home<\/a>\
+                <\/div>\
+            <\/div>\
+        <\/div>";  
+    },
+    
     // Create a form to view and edit the description
     displayImageDescription(weatherDescription, geolocationDescription) {
         this.clearImageDescription();
