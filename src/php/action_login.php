279718eff4078e1c03555efa291ef5e88584b332
Sean Jones <seanjone@buffalo.edu>
2020-10-21 19:53:45 -0400

    User Story #1 - Use cookie to display logged in user in navbar

--- src/php/action_login.php
+++ src/php/action_login.php
@@ -30,7 +30,7 @@ if(isset($_POST['email'], $_POST['psw'])){
             //initialize cookie to store username
             $cookie_name = 'username';
             $cookie_value = $email;
-            setcookie($cookie_name,$cookie_value);
+            setcookie($cookie_name, $cookie_value, time() + (86400*30),'/');
             //update sql database to show user logged in
             mysqli_query($conn, "UPDATE `user_table` SET login_status = 1 WHERE email = '$email'");
             header('location: ../index.html');
