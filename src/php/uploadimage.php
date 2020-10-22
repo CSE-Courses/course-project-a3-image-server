248ac054b61a9dafd11a03f00f133807f2fdd0b2
Sean Jones <seanjone@buffalo.edu>
2020-10-21 23:01:45 -0400

    Task #42 - Comment out extension check in uploadimage.php

--- src/php/uploadimage.php
+++ src/php/uploadimage.php
@@ -15,25 +15,33 @@
         //file size should be less than 4 MB
         $_SESSION['message'] = "File size is too big. Please limit to 4MB.";
         //header call back to home
+        header('location: ../index.html');
         exit();
     }
-
+/*
     //check for file type
-    if($fileType != 'image/jpeg' && $fileType != 'image/jpg' && $fileType != 'image/png'){
+    if($fileType != 'image/jpeg' && $fileType != 'image/JPG' && $fileType != 'image/png'){
+        fwrite($debug, "HERE");
         //file type should be jpeg,jpg, or png
         $_SESSION['message'] = "Wrong file type";
         //header call
+        header('location: ../index.html');
         exit();
     }
+*/
 
-    $fileUpload = "./tmp_store/".$_FILES['file_upload']['name'].$_SESSION['email'];
+    $fileUpload = "../tmp_store/".$_SESSION['email'];
 
     //move image to tmo folder
     if(!move_uploaded_file($_FILES['file_upload']['tmp_name'], $fileUpload)){
         $_SESSION['message'] = "Could not move file to destination";
         //header here
+        header('location: ../index.html');
         exit();
     }
+
+    header('location: ../index.html');
+    exit();
 
 
 
