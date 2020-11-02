25f6460bd2aa71dbeb9b36fdc1bd557fe11d68fe
Sean Jones <seanjone@buffalo.edu>
2020-11-02 18:21:50 -0500

    Task #42 - Use exif_read_data to extract metadata from uploaded image in tmp_store folder

--- src/php/processimage.php
+++ src/php/processimage.php
@@ -3,4 +3,18 @@
  * Purpose: Process image that has been uploaded via upload image
  */
 
+// Open a the file in binary mode
+$image = fopen('../tmp_store/##NAME', 'rb');
 
+if (!$image) {
+    echo 'Error: Unable to open image for reading';
+    exit;
+}
+
+// Attempt to read the exif headers
+$headers = exif_read_data($image);
+
+if (!$headers) {
+    echo 'Error: Unable to read exif headers';
+    exit;
+}
