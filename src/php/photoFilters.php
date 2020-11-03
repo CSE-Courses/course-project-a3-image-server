567b767db75d709bbfa1999cc7d8cdac8102075a
Nick Anzalone <nickanzalone@nicks-air.myfiosgateway.com>
2020-11-03 09:55:58 -0500

    created php functions to apply photo filters

---
+++ src/php/photoFilters.php
@@ -0,0 +1,42 @@
+<?php
+require 'vendor/autoload.php';
+
+$imagine = new Imagine\Gd\Imagine();
+
+//open image and apply the effects and then save the image. 
+function negativeFilter(){
+$imagine = new Imagine\Gd\Imagine();
+
+$image = $imagine->open('../tmp_store/test.jpeg');
+$image->effects()->negative();
+$image->rotate(90);
+
+
+$image->save('negative.png');
+}
+
+
+function sharpenFilter(){
+$imagine = new Imagine\Gd\Imagine();
+
+$image = $imagine->open('../tmp_store/test.jpeg');
+
+$image->effects()->sharpen();
+$image->rotate(90);
+$image->save('sharpen.png');
+}
+
+function grayscaleFilter(){
+$imagine = new Imagine\Gd\Imagine();
+
+
+$image = $imagine->open('../tmp_store/test.jpeg');
+
+$image->effects()->grayscale();
+$image->rotate(90);
+
+$image->save('grayscale.png');
+}
+
+
+?>
