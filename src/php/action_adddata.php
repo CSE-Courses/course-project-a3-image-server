4ab0149a2d661d92b668af048a89543c816bba8e
Sean Jones <seanjone@buffalo.edu>
2020-12-01 12:34:15 -0500

    Task #57 - Add in sql statements to either add column if it does not exist or update column if it does.

--- src/php/action_adddata.php
+++ src/php/action_adddata.php
@@ -11,9 +11,24 @@
         global $conn; // global connection variable to database
         include 'connect_db.php'; // connect to database
 
+        $email = $_SESSION['email'];
+
         $colName = $_SESSION['insert_col_name']; // tag for data to insert
         $colData = $_SESSION['insert_data']; // data to insert into database
         $updateFile = $_SESSION['filename_to_update']; // should be file relative to php directory (going to load right from this value)
+
+        $col_qry = "SELECT `$colName` FROM `imagestore`";
+        $col_query = mysqli_query($conn, $col_qry);
+        $num_rows = mysqli_num_rows($col_query);
+        if($num_rows){
+            //if column exists just update data
+            $update = "UPDATE `imagestore` SET '$colName' = '$colData' WHERE email = '$email'";
+            $update_qry = mysqli_query($conn, $update);
+        } else {
+            //if not, add column and insert data
+            $add_col = "ALTER TABLE `imagestore` ADD '$colName' varchar(255)";
+            $add_qry = mysqli_query($conn, $add_col);
+        }
 
         //message for successful addition of data
         $_SESSION['message'] = 'Data addition success';
