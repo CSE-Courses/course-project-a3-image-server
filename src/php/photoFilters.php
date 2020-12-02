8bf2f2d69b6e41d1a41b382cbb05146bab070961
Nick Anzalone <nickanzalone@Nicks-Air.home>
2020-12-01 22:06:21 -0500

    Task #61 - now takes image that user uploads instead of default test image in tmp directory

--- src/php/photoFilters.php
+++ src/php/photoFilters.php
@@ -4,48 +4,152 @@ require 'vendor/autoload.php';
 $imagine = new Imagine\Gd\Imagine();
 
 //open image and apply the effects and then save the image. 
-function negativeFilter(){
+function negativeFilter($filePath){
 $imagine = new Imagine\Gd\Imagine();
 
-$image = $imagine->open('../tmp_store/test.jpeg');
+$image = $imagine->open($filePath);
 $image->effects()->negative();
 $image->rotate(90);
+$out = '../tmp_store/download.png';
+file_put_contents($out, $image);
+if(file_exists($out)){
+$fileName = basename($out);
+$fileSize = filesize($out);
+header('Content-Description: File Transfer');
+header("Content-Type: image/jpeg");
+header("Content-Disposition: attachment; filename=".$fileName);
+header('Expires: 0');
+header('Cache-Control: must-revalidate');
+header('Pragma: public');
+header("Content-Length: " . $fileSize);
+readfile($out);
+exit;
+}else{
+echo '<script>alert("The file/files can not be downloaded")</script>'; 
 
-
-$image->save('../tmp_store/negDownload.png');
+}
 }
 
-
-function sharpenFilter(){
+function sharpenFilter($filePath){
 $imagine = new Imagine\Gd\Imagine();
 
-$image = $imagine->open('../tmp_store/test.jpeg');
+$image = $imagine->open($filePath);
 
 $image->effects()->sharpen();
 $image->rotate(90);
-$image->save('../tmp_store/sharpDownload.png');
-}
+$out = '../tmp_store/download.png';
+file_put_contents($out, $image);
+if(file_exists($out)){
+$fileName = basename($out);
+$fileSize = filesize($out);
+header('Content-Description: File Transfer');
+header("Content-Type: image/jpeg");
+header("Content-Disposition: attachment; filename=".$fileName);
+header('Expires: 0');
+header('Cache-Control: must-revalidate');
+header('Pragma: public');
+header("Content-Length: " . $fileSize);
+readfile($out);
+exit;
+}else{
+echo '<script>alert("The file/files can not be downloaded")</script>'; 
 
-function grayscaleFilter(){
+}
+}
+function grayscaleFilter($filePath){
 $imagine = new Imagine\Gd\Imagine();
 
 
-$image = $imagine->open('../tmp_store/test.jpeg');
+$image = $imagine->open($filePath);
 
 $image->effects()->grayscale();
 $image->rotate(90);
+$out = '../tmp_store/download.png';
+file_put_contents($out, $image);
+if(file_exists($out)){
+$fileName = basename($out);
+$fileSize = filesize($out);
+header('Content-Description: File Transfer');
+header("Content-Type: image/jpeg");
+header("Content-Disposition: attachment; filename=".$fileName);
+header('Expires: 0');
+header('Cache-Control: must-revalidate');
+header('Pragma: public');
+header("Content-Length: " . $fileSize);
+readfile($out);
+exit;
+}else{
+echo '<script>alert("The file/files can not be downloaded")</script>'; 
 
-$image->save('../tmp_store/grayDownload.png');
 }
+}
+
+    session_start();
+    if(isset($_SESSION['email'])){
+        //extract post variables
+        extract($_POST);
+
+        //change this based on file name
+        $fileType = $_FILES['file_upload']['type'];
+        $fileSize = $_FILES['file_upload']['size'];
+        $fileName = $_FILES['file_upload']['name'];
+        //set session variables so these can be accessed in processimage
+        $_SESSION['file_upload_type'] = $fileType;
+        $_SESSION['file_upload_size'] = $fileSize;
+        $_SESSION['file_upload_name'] = $fileName;
+
+        if($fileSize/1024 > "4096"){
+            //file size should be less than 4 MB
+            $_SESSION['message'] = "File size is too big. Please limit to 4MB.";
+            //header call back to home
+            header('location: ../index.html');
+            exit();
+        }
+
+        //check for file type
+        if($fileType != 'image/jpeg' && $fileType != 'image/JPG' && $fileType != 'image/png'){
+            //file type should be jpeg,jpg, or png
+            $_SESSION['message'] = "Wrong file type";
+            //header call
+            header('location: ../index.html');
+            ?>
+            <script>alert("Submission Error");</script>
+            <?php
+            exit();
+        }
+
+        //check if user directory exists and make one if not
+        $userDir = "../tmp_store/".$_SESSION['email'];
+        if(!is_dir($userDir)){
+            mkdir($userDir);
+        }
+
+        $fileUpload = "../tmp_store/".$_SESSION['email']."/".$_FILES['file_upload']['name'].date("His");
+        //Define session variable to store tmp_name
+        $_SESSION['tmp_name'] = $fileUpload;
+
+        //move image to tmp folder
+        if(!move_uploaded_file($_FILES['file_upload']['tmp_name'], $fileUpload)){
+            $_SESSION['message'] = "Could not move file to destination";
+            //header here
+            header('location: ../index.html');
+            exit();
+        }
+    } else {
+        $_SESSION['message'] = "Not Logged In";
+        //header here
+        header('location: ../index.html');
+    }
+
 
 if(isset($_POST['grayscale'])){
-grayscaleFilter();
+grayscaleFilter($SESSION['tmp_name']);
 }
 if(isset($_POST['negative'])){
-negativeFilter();
+negativeFilter($SESSION['tmp_name']);
 }
 if(isset($_POST['sharpen'])){
-sharpenFilter();
+sharpenFilter($SESSION['tmp_name']);
 }
 
 
